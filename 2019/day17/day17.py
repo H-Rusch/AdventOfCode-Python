e193225a285154f989b1384c72928155b6194df3
H-Rusch <hendrik.rusch1@uni-oldenburg.de>
2022-04-08 16:29:29 +0200

    day18 - part 2

--- 2019/day17/day17.py
+++ 2019/day17/day17.py
@@ -143,7 +143,7 @@ class IntcodeV3_3:
 
     def output_value(self, parameters: list):
         self.output = parameters[0]
-        self.running = 2
+        self.running = 3
         # print("Diagnostic output:", self.output[-1])
 
     def jump_if_true(self, parameters: list):
@@ -193,15 +193,128 @@ class IntcodeV3_3:
 # --- actual functionality ----------------------------------------------
 def part_1(instructions: list):
     tiles = generate_tile_map(instructions)
-    print_scaffold(tiles)
+    # print_scaffold(tiles)
     intersections = find_intersections(tiles)
 
     return sum(map(lambda c: c[0] * c[1], intersections))
 
 
 def part_2(instructions: list):
-    pass
+    tiles = generate_tile_map(instructions)
+    # print_scaffold(tiles)
+    movement = find_path(tiles)[1:]
+    """
+    manually compressing the instruction list:
+    R, 10, R, 8, L, 10, L, 10, R, 8, L, 6, L, 6, R, 8, L, 6, L, 6, R, 10, R, 8, L, 10, L, 10, L, 10, R, 10, L, 6, R, 8, L, 6, L, 6, L, 10, R, 10, L, 6, L, 10, R, 10, L, 6, R, 8, L, 6, L, 6, R, 10, R, 8, L, 10, L, 10
+    =>
+    A, B, B, A, C, B, C, C, B, A
+        
+    A: R, 10, R, 8, L, 10, L, 10 
+    B: R, 8, L, 6, L, 6
+    C: L, 10, R, 10, L, 6
+    """
+    instructions[0] = 2
+    computer = IntcodeV3_3(instructions)
+    movement = "A,B,B,A,C,B,C,C,B,A\nR,10,R,8,L,10,L,10\nR,8,L,6,L,6\nL,10,R,10,L,6\nn\n"
+    i = 0
 
+    # computer.execute_program()
+    while i < len(movement):
+        computer.input = ord(movement[i])
+        i += 1
+        computer.execute_program()
+        while computer.running == 3:
+            computer.execute_program()
+
+    while computer.running != 0:
+        computer.execute_program()
+
+    return computer.output
+
+
+def find_path(tiles: dict) -> list:
+    x, y = 0, 0
+    # 0: up, 1: down, 2: left, 3: right
+    direction = 0
+    for (dx, dy), tile in tiles.items():
+        if tile in ["^", "v", "<", ">"]:
+            x, y = dx, dy
+            direction = ["^", "v", "<", ">"].index(tile)
+            break
+
+    instructions = []
+    steps = 0
+    while True:
+        # try to go forward
+        dx, dy = get_forward(x, y, direction)
+        if tiles.get((dx, dy), ".") == "#":
+            steps += 1
+            x, y = dx, dy
+        else:
+            instructions.append(str(steps))
+            # if forward is empty, try right and if that fails try left
+            if tiles.get(get_right(x, y, direction), ".") == "#":
+                steps = 0
+                instructions.append("R")
+                direction = clockwise(direction)
+            elif tiles.get(get_left(x, y, direction), ".") == "#":
+                steps = 0
+                instructions.append("L")
+                direction = counter_clockwise(direction)
+            else:
+                break
+
+    return instructions
+
+def get_forward(x: int, y: int, direction: int) -> tuple[int, int]:
+    if direction == 0:
+        return x, y - 1
+    elif direction == 1:
+        return x, y + 1
+    elif direction == 2:
+        return x - 1, y
+    elif direction == 3:
+        return x + 1, y
+
+def get_right(x: int, y: int, direction: int) -> tuple[int, int]:
+    if direction == 0:
+        return x + 1, y
+    elif direction == 1:
+        return x - 1, y
+    elif direction == 2:
+        return x, y - 1
+    elif direction == 3:
+        return x, y + 1
+
+def get_left(x: int, y: int, direction: int) -> tuple[int, int]:
+    if direction == 0:
+        return x - 1, y
+    elif direction == 1:
+        return x + 1, y
+    elif direction == 2:
+        return x, y + 1
+    elif direction == 3:
+        return x, y - 1
+
+def clockwise(d: int) -> int:
+    if d == 0:
+        return 3
+    if d == 1:
+        return 2
+    if d == 2:
+        return 0
+    if d == 3:
+        return 1
+
+def counter_clockwise(d: int) -> int:
+    if d == 0:
+        return 2
+    if d == 1:
+        return 3
+    if d == 2:
+        return 1
+    if d == 3:
+        return 0
 
 def get_adjacent(x: int, y: int) -> list:
     return [(x + dx, y + dy) for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]]
@@ -239,7 +352,6 @@ def find_intersections(tiles: dict):
             if all([tiles.get((dx, dy), "") in scaffold_symbols for (dx, dy) in get_adjacent(x, y)]):
                 intersections.append((x, y))
 
-    print(intersections)
     return intersections
 
 
@@ -269,4 +381,4 @@ if __name__ == "__main__":
 
     print(f"Part 1: The sum of all alignment parameters is {part_1(numbers)}.")
 
-    print(f"Part 2: {part_2(numbers)} .")
+    print(f"Part 2: The vacuum has {part_2(numbers)} dust-units collected.")
