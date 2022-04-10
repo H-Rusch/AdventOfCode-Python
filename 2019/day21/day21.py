7b4a5a1554038831a80dcd67de1a1f665f439336
H-Rusch <hendrik.rusch1@uni-oldenburg.de>
2022-04-10 13:21:48 +0200

    day21

---
+++ 2019/day21/day21.py
@@ -0,0 +1,259 @@
+class IntcodeV3_4:
+    def __init__(self, program: list):
+        self.memory = program[:]
+        for _ in range(10_000):
+            self.memory.append(0)
+        self.running = 1
+        self.pc = 0
+        self.relative_base = 0
+
+        self.output = []
+        self.input = []
+
+    def execute_program(self):
+        self.running = 1
+        while self.running == 1:
+            opcode = str(self.memory[self.pc])
+
+            self.parse_opcode(opcode.zfill(5))
+
+    # Decoding instructions --------------
+    def parse_opcode(self, opcode: str):
+        op = int(opcode[-2:])
+        parameter_modes = list(reversed(opcode[:3]))
+        if op == 99:
+            self.running = 0
+
+        # add and mult
+        elif op in [1, 2]:
+            parameters = [self.parse_parameter_freely(parameter_modes[0], self.memory[self.pc + 1]),
+                          self.parse_parameter_freely(parameter_modes[1], self.memory[self.pc + 2]),
+                          self.parse_parameter_restricted(parameter_modes[2], self.memory[self.pc + 3])]
+
+            if op == 1:
+                self.add(parameters)
+
+            elif op == 2:
+                self.mult(parameters)
+
+            self.pc += 4
+
+        # input and output
+        elif op in [3, 4]:
+            if op == 3:
+                if len(self.input) != 0:
+                    user_input = self.input.pop(0)
+                else:
+                    self.running = 2
+                    return
+                # user_input = int(input("Which value to save?: "))
+                parameters = [user_input,
+                              self.parse_parameter_restricted(parameter_modes[0], self.memory[self.pc + 1])]
+
+                self.write_value(parameters)
+
+            else:
+                parameters = [self.parse_parameter_freely(parameter_modes[0], self.memory[self.pc + 1])]
+
+                self.output_value(parameters)
+
+            self.pc += 2
+
+        # jumps
+        elif op in [5, 6]:
+            parameters = [self.parse_parameter_freely(parameter_modes[0], self.memory[self.pc + 1]),
+                          self.parse_parameter_freely(parameter_modes[1], self.memory[self.pc + 2])]
+
+            if op == 5:
+                self.jump_if_true(parameters)
+
+            else:
+                self.jump_if_false(parameters)
+
+        # comparisons
+        elif op in [7, 8]:
+            parameters = [self.parse_parameter_freely(parameter_modes[0], self.memory[self.pc + 1]),
+                          self.parse_parameter_freely(parameter_modes[1], self.memory[self.pc + 2]),
+                          self.parse_parameter_restricted(parameter_modes[2], self.memory[self.pc + 3])]
+
+            if op == 7:
+                self.less_than(parameters)
+
+            else:
+                self.equals(parameters)
+
+            self.pc += 4
+
+        # adjust relative base
+        elif op == 9:
+            parameters = [self.parse_parameter_freely(parameter_modes[0], self.memory[self.pc + 1])]
+
+            self.adjust_relative_base(parameters)
+
+            self.pc += 2
+
+        else:
+            raise Exception("Instruction unknown:", opcode)
+
+    # concrete implementation of functionality ----------------
+    def parse_parameter_freely(self, parameter_mode: str, value: int) -> int:
+        if parameter_mode == "0":
+            return self.memory[value]
+
+        elif parameter_mode == "1":
+            return value
+
+        elif parameter_mode == "2":
+            return self.memory[value + self.relative_base]
+
+        else:
+            raise Exception("Parameter Mode unknown")
+
+    def parse_parameter_restricted(self, parameter_mode: str, value: int) -> int:
+        """ To be used when an address value should be given for a parameter. """
+        if parameter_mode in ["0", "1"]:
+            return value
+
+        elif parameter_mode == "2":
+            return value + self.relative_base
+
+        else:
+            raise Exception("Parameter Mode unknown")
+
+    def add(self, parameters: list):
+        value1 = parameters[0]
+        value2 = parameters[1]
+        destination = parameters[2]
+
+        self.memory[destination] = value1 + value2
+
+    def mult(self, parameters: list):
+        value1 = parameters[0]
+        value2 = parameters[1]
+        destination = parameters[2]
+
+        self.memory[destination] = value1 * value2
+
+    def write_value(self, parameters: list):
+        value = parameters[0]
+        adr = parameters[1]
+
+        self.memory[adr] = value
+
+    def output_value(self, parameters: list):
+        self.output.append(parameters[0])
+        # self.running = 3
+        # print("Diagnostic output:", self.output[-1])
+
+    def jump_if_true(self, parameters: list):
+        param1 = parameters[0]
+        param2 = parameters[1]
+
+        if param1 != 0:
+            self.pc = param2
+        else:
+            self.pc += 3
+
+    def jump_if_false(self, parameters: list):
+        param1 = parameters[0]
+        param2 = parameters[1]
+
+        if param1 == 0:
+            self.pc = param2
+        else:
+            self.pc += 3
+
+    def less_than(self, parameters: list):
+        param1 = parameters[0]
+        param2 = parameters[1]
+        adr = parameters[2]
+
+        if param1 < param2:
+            self.memory[adr] = 1
+        else:
+            self.memory[adr] = 0
+
+    def equals(self, parameters: list):
+        param1 = parameters[0]
+        param2 = parameters[1]
+        adr = parameters[2]
+
+        if param1 == param2:
+            self.memory[adr] = 1
+        else:
+            self.memory[adr] = 0
+
+    def adjust_relative_base(self, parameters: list):
+        value = parameters[0]
+
+        self.relative_base += value
+
+
+# --- code for day 21 --------------------------------------------------------------------------
+def part_1(instructions: list) -> int:
+    computer = IntcodeV3_4(instructions)
+
+    computer.execute_program()
+    computer.output = []
+
+    program = ["OR A T\n",
+               "AND B T\n",
+               "AND C T\n",
+               "NOT T J\n",
+               "AND D J\n",
+               "WALK\n"]
+    program = [ord(c) for c in "".join(program)]
+    computer.input = program
+    computer.execute_program()
+
+    last = computer.output.pop()
+    print_output(computer.output)
+
+    return last
+
+
+def part_2(instructions: list) -> int:
+    computer = IntcodeV3_4(instructions)
+
+    computer.execute_program()
+    computer.output = []
+
+    # first part: 4 spaces in front is ground. in the 3 spaces in front is a hole
+    # second part: 8 spaces in front is ground or 5 spaces in front is ground to continue after jumping.
+    program = ["OR A T\n",
+               "AND B T\n",
+               "AND C T\n",
+               "NOT T J\n",
+               "AND D J\n",
+
+               "AND J T\n",
+               "OR E T\n",
+               "OR H T\n",
+               "AND T J\n",
+
+               "RUN\n"]
+    program = [ord(c) for c in "".join(program)]
+    computer.input = program
+    computer.execute_program()
+
+    last = computer.output.pop()
+    print_output(computer.output)
+
+    return last
+
+
+def print_output(numbers: list):
+    print("".join([chr(n) for n in numbers]))
+
+
+def parse_input():
+    with open("input.txt", "r") as file:
+        return [int(n) for n in file.read().split(",")]
+
+
+if __name__ == "__main__":
+    numbers = parse_input()
+
+    print(f"Part 1: The droid reports a hull damage of {part_1(numbers)} while walking.")
+
+    print(f"Part 2: The droid reports a hull damage of {part_2(numbers)} while running.")
