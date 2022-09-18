633b6fae90f890fac0f73c7262e96223841ccee6
H-Rusch <kirdneh98mail@gmail.com>
2022-09-18 16:21:34 +0200

    2018 day 1-3

---
+++ 2018/day01/day01.py
@@ -0,0 +1,28 @@
+def part_1(changes: list) -> int:
+    return sum([int(s) for s in changes])
+
+
+def part_2(changes: list) -> int:
+    seen_frequencies = set()
+    current = 0
+    seen_frequencies.add(current)
+
+    while True:
+        for change in changes:
+            current += int(change)
+            if current in seen_frequencies:
+                return current
+            seen_frequencies.add(current)
+
+
+def parse_input() -> list:
+    with open("input.txt", "r") as file:
+        return file.readlines()
+
+
+if __name__ == "__main__":
+    values = parse_input()
+
+    print(f"Part 1: The resulting frequency is {part_1(values)}.")
+
+    print(f"Part 2: The first frequency which is reached twice is {part_2(values)}.")
