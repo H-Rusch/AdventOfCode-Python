1d6a13a632cd913fae66982684e1fa2a2ba13c34
H-Rusch <kirdneh98mail@gmail.com>
2023-10-10 21:26:07 +0200

    2017 day 8

---
+++ aoc2017/solutions/day08.py
@@ -0,0 +1,60 @@
+from dataclasses import dataclass
+from collections import defaultdict
+import re
+
+
+INSTRUCTION_PATTERN = re.compile(r"(.*?) (dec|inc) (-?\d+) if (.*?) (.*)")
+
+
+@dataclass
+class Instruction:
+    register: str
+    # value handles both increasing and decreating values
+    value: int
+    condition_register: str
+    condition: str
+
+
+def part1(input):
+    instructions = parse(input)
+    registers, _ = perform_instructions(instructions)
+    
+    return highest_register_value(registers)
+
+
+def part2(input):
+    instructions = parse(input)
+    _, max_value = perform_instructions(instructions)
+    
+    return max_value
+
+
+def perform_instructions(instructions: list) -> dict[str, int]:
+    registers = defaultdict(int)
+    max_value = 0
+
+    for instruction in instructions:
+        if eval(f"registers[instruction.condition_register] " + instruction.condition):
+            registers[instruction.register] += instruction.value
+
+            max_value = max(max_value, highest_register_value(registers))
+
+    return registers, max_value
+
+
+def highest_register_value(registers: dict[str, int]) -> int:
+    return max(registers.values())
+
+
+def parse(input: str) -> list[Instruction]:
+    instructions = []
+    for line in input.splitlines():
+        register, operation, value, condition_register, condition = INSTRUCTION_PATTERN.match(line).groups()
+
+        match (operation):
+            case "inc": value = int(value)
+            case "dec": value = -int(value)
+
+        instructions.append(Instruction(register, value, condition_register, condition))
+
+    return instructions
---
+++ aoc2017/tests/test_day08.py
@@ -0,0 +1,13 @@
+from ..solutions import day08
+
+
+EXAMPLE_INPUT = """b inc 5 if a > 1
+a inc 1 if b < 5
+c dec -10 if a >= 1
+c inc -20 if c == 10"""
+
+def test_part1_example():
+    assert 1 == day08.part1(EXAMPLE_INPUT)
+
+def test_part2_example():
+    assert 10 == day08.part2(EXAMPLE_INPUT)
