a7e73246ebae084c0b7a05f3cd32e1efc5d4a3ea
H-Rusch <kirdneh98mail@gmail.com>
2023-10-11 22:10:46 +0200

    2020 day 7 Refactoring

--- aoc2020/solutions/day07.py
+++ aoc2020/solutions/day07.py
@@ -1,72 +1,73 @@
 import re
 
 
+BAG_NUMBER_PATTERN = re.compile(r"(no|\d) (\w* \w*) bag")
+
+
 def part1(input):
-    rules = parse(input)
-    rule_dict = {}
+    bag_contents = parse(input)
 
-    bag_regex = re.compile("(\w* \w*) bag")
+    return count_possibilities(bag_contents, "shiny gold")
+
+
+def part2(input):
+    rule_dict = parse(input)
+
+    # count recursively
+    return count_bags("shiny gold", rule_dict)
+
+
+def count_possibilities(bag_contents: dict, initial_search: str) -> int:
     possibilities = 0
-
-    for rule in rules:
-        match = bag_regex.findall(rule)
-        rule_dict[match[0]] = match[1:]
-
-    searching_for = ["shiny gold"]
     parents = set()
+    searching = [initial_search]
 
-    while searching_for:
+    while len(searching) > 0:
         # search parents of an element we are searching for by looking up in which entry's list it is
-        searching = searching_for.pop()
-        for entry in rule_dict:
-            if searching in rule_dict[entry]:
-                parents.add(entry)
+        current = searching.pop()
+
+        for bag, bag_content in bag_contents.items():
+            contained_bags = {bag for (_, bag) in bag_content}
+            if current in contained_bags:
+                parents.add(bag)
 
         # remove the parents from the dictionary
-        for entry in parents:
-            rule_dict.pop(entry)
+        for bag in parents:
+            bag_contents.pop(bag)
 
         # update the number of possibilities by the number of parent bags
         possibilities += len(parents)
 
-        # add parents to the searching_for list, so their parents can also be found
-        searching_for.extend(parents)
+        # expand search to the parents of the current bag
+        searching.extend(parents)
         parents.clear()
 
     return possibilities
 
 
-def part2(input):
-    rules = parse(input)
-    rule_dict = {}
-
-    bag_num_regex = re.compile("(no|\d) (\w* \w*) bag")
-    # create rule dictionary. The key is the bag outside and for each bag inside an entry in a list is made.
-    # That list is the value
-
-    for rule in rules:
-        bag_outside = rule[:rule.find("bags") - 1]
-        bags = bag_num_regex.findall(rule)
-        rule_dict[bag_outside] = bags
-
-    # call recursive method
-    return count_bags("shiny gold", rule_dict)
-
-
 def count_bags(bag_name: str, rule_dict: dict):
-    # list of bags inside with the information how often they are present
-    bags = rule_dict[bag_name]
-    # bag has no other bags inside
-    if not bags:
+    contained_bags = rule_dict[bag_name]
+
+    if len(contained_bags) == 0:
         return 0
+
     # First add how often the bag is present. Then add how many bags are in children bags
     bag_count = 0
-    for bag in bags:
-        bag_count += int(bag[0])
-        bag_count += int(bag[0]) * count_bags(bag[1], rule_dict)
+    for n, bag_name in contained_bags:
+        bag_count += n
+        bag_count += n * count_bags(bag_name, rule_dict)
 
     return bag_count
 
 
-def parse(input):
-    return input.splitlines()
+def parse(input: str) -> dict[str, list[(int, str)]]:
+    bag_contents = {}
+
+    for rule in input.splitlines():
+        bag = rule[: rule.find("bags") - 1]
+        contained_bags = [
+            (int(n), description) for n, description in BAG_NUMBER_PATTERN.findall(rule)
+        ]
+        bag_contents[bag] = contained_bags
+
+    return bag_contents
